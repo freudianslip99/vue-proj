a5fcdb46981c23027c3197d1ed09606387fa8e6e
Alan B. Carpenter <uac@studentbox.ornl.gov>
2018-06-21 10:29:13 -0400

    removed css

--- src/components/Skills.vue
+++ src/components/Skills.vue
@@ -18,9 +18,7 @@ export default {
           { "skill": "Vue.js" },
           { "skill": "Frontend Developer" }
       ],
-     bgColor: 'blue', 
-     bgWidth: '100%',
-     bgHeight: '30px'
+     
      }
     }
   }
@@ -29,33 +27,6 @@ export default {
 
 
 <style scoped>
-  .holder {
-    background: #fff;
-  }
-
-  ul {
-    margin: 0;
-    padding: 0;
-    list-style-type: none;
-  }
   
-  ul li {
-    padding: 20px;
-    font-size: 1.3em;
-    background-color: #E0EDF4;
-    border-left: 5px solid #3EB3F6;
-    margin-bottom: 2px;
-    color: #3E5252;
-  }
-
-  p {
-    text-align:center;
-    padding: 30px 0;
-    color: gray;
-  }
-
-  .container {
-    box-shadow: 0px 0px 40px lightgray;
-  }
 
   </style>
