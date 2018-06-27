11049d1570af491d1d64e85e1ea0c961a632cc3a
Alan <alan.tnpsych@gmail.com>
2018-06-27 12:00:48 -0400

    Update to Nav

---
+++ src/components/Nav.vue
@@ -0,0 +1,27 @@
+<template>
+<div>
+<v-navigation-drawer v-model="drawer" fixed app>
+    <v-list dense>
+    </v-list>
+</v-navigation-drawer>
+<v-toolbar color="indigo" dark fixed app>
+    <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+    <v-toolbar-title>{{message}}</v-toolbar-title>
+</v-toolbar>
+</div>
+</template>
+
+<script>
+export default {
+    name: "navigation",
+    data() {
+        return {
+            message: "This will will be the Navigation",
+            drawer: null
+        }
+    }
+}
+</script>
+
+<style>
+</style>
