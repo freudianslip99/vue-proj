d27ae4e51bb16a779a965fdc24108863dce76802
Alan <alan.tnpsych@gmail.com>
2018-06-27 15:02:02 -0400

    Update Graph.vue

--- src/components/Graph.vue
+++ src/components/Graph.vue
@@ -3,20 +3,7 @@
 </template>
 
 <script>
-  created: () ({
-    eventBus.$on('Toolssaid', (message) => {
-      this.fromTools = message
-      }
-    }),
   
-
-  data: () 
-    return {
-      thecardtitle: 'Child Component!',
-      fromsister: '';
-      
-    }
-  }
 </script>
 
 <style>
