acf7088623949e93ea7d9e289c48c1dcf425c031
Alan <alan.tnpsych@gmail.com>
2018-06-27 15:02:46 -0400

    Update Tools.vue

--- src/components/Tools.vue
+++ src/components/Tools.vue
@@ -1,17 +1,10 @@
 <template>
-  <div v-if="fromGraph" class="mt-3 alert alert-secondary" v-html="fromGraph"></div>
+  
 
 </template>
 
 <script>
- import {eventBus} from "../main.js";
 
- methods: {
-    messageGraph() {
-        eventBus.$emit('Toolssaid', 'Tools said do your homework!');
-    
-}
-};
 </script>
 
 
