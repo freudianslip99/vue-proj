c44ade109ae5117f2d1464bdcd402b452b5a8ba3
Alan <alan.tnpsych@gmail.com>
2018-06-27 13:13:35 -0400

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,9 +1,13 @@
 import Vue from 'vue'
+import Vuetify from 'vuetify'
 import App from './App.vue'
-
-Vue.config.productionTip = false
+import 'vuetify/dist/vuetify.min.css'
+import 'material-design-icons-iconfont/dist/material-design-icons.css'
 
 export const eventBus = new Vue();
+
+Vue.use(Vuetify)
+Vue.config.productionTip = false
 
 new Vue({
   el: '#app',
